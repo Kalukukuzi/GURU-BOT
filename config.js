91f9b1b5124f97256d561250fbbc65ef0f017647
Kalukukuzi <98535730+Kalukukuzi@users.noreply.github.com>
2023-11-25 13:38:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349011892837', 'NovaXbot2', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
